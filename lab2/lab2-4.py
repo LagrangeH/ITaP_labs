7be6365f5a380705f3475104d5c8c779a1b8cd31
LagrangeH <maxsergeev39@gmail.com>
2022-09-10 00:42:49 +0300

    Fix and simplify

--- lab2/lab2-4.py
+++ lab2/lab2-4.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3.10
 
-def main(*x):
-    user_input = input("Enter data for processing separated by a space: ")
+def main():
+    user_input = input("Enter data for processing: ")
 
-    list3 = user_input.split()
+    list3 = list(user_input)
     print(f"{list3 = }")
 
     len_list3 = len(list3)
     print(f"{len_list3 = }")
 
-    count_x = sum(list3.count(string) for string in x)
-    print(f"For x = {list(x)} {count_x = }")
+    count_x = sum(list3.count(string) for string in ('2', '3'))
+    print(f"2 and 3 contains in list3 {count_x} times")
 
 
 if __name__ == '__main__':
-    # Since the task does not say what exactly to count,
-    # we will grant such a right to the user.
-    # We also use set to avoid recalculating the same value.
-    main(*set(input("Enter data for counting separated by a space: ").split(r'\s')))
+    main()

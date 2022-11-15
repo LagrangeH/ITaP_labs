edd6e408908e69da8523662e5aa77a289fb78bec
LagrangeH <maxsergeev39@gmail.com>
2022-11-15 16:07:57 +0300

    Refactoring to use Jupyter

--- labs/lab12/lab12_1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3.10
-
-def ssh_conn(ip_address, username, password):
-    print(f"{ip_address = }\n{username = }\n{password = }")
-
-
-if __name__ == '__main__':
-    ssh_conn(ip_address='192.168.0.10', username='root', password='toor')
-    ssh_conn('192.168.0.10', username='admin', password='adminadmin')
--- labs/lab12/lab12_4.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3.10
-
-if __name__ == '__main__':
-    nums = (45, 36, 39, 37, 130, 105, 220, 169)
-    e = lambda x: not x % 13
-    print([x for x in nums if e(x)])
